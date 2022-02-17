9e2d1583a5c91e594f7b12d0f18232c04d3bceaa
Claudio <claudio.junior233@gmail.com>
2022-02-17 13:41:30 -0300

    ex 1 - 2 -3

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
-﻿
+﻿// Ex1
+// HelloWorld(); 
+
+// Ex2
+// TypeOfSomething();
+
+//Ex 3
+// CompleteName();
 
 void HelloWorld(){
     Console.WriteLine("Hello World");
 };
 
-HelloWorld();
+void TypeOfSomething(){
+
+    var nome = "Claudio";
+
+    Console.WriteLine(nome.GetType());
+};
+
+void CompleteName(){
+    Console.WriteLine("Olá, qual é seu nome?");
+    var nome = Console.ReadLine();
+    Console.WriteLine("E seu sobrenome?");
+    var sobrenome = Console.ReadLine();
+    Console.WriteLine($"Seja bem vindo {nome} {sobrenome}!");
+    
+};
+
